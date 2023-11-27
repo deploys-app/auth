2d6de756cd746831cf3ad1a6def1408760951098
Thanatat Tamtan <acoshift@gmail.com>
2023-11-27 14:55:13 +0700

    add support google token

--- src/info.js
+++ src/info.js
@@ -11,8 +11,17 @@ export default async function info (request, env, ctx) {
 	if (!token) {
 		return failResponse('auth: unauthorized')
 	}
-	const hashedToken = await hash(token)
-	const tokenInfo = await getTokenInfo(env, hashedToken)
+
+	/** @type {?TokenInfo} */
+	let tokenInfo = null
+
+	if (token.startsWith('deploys-api.')) {
+		const hashedToken = await hash(token)
+		tokenInfo = await getTokenInfo(env, hashedToken)
+	} else if (token.startsWith('ya29.')) {
+		tokenInfo = await getGoogleTokenInfo(env, token)
+	}
+
 	if (!tokenInfo) {
 		return failResponse('auth: unauthorized')
 	}
@@ -86,5 +95,30 @@ async function getTokenInfo (env, hashedToken) {
 	return {
 		email: r.email,
 		clientId: r.client_id
+	}
+}
+
+/**
+ * getGoogleTokenInfo gets the tokenInfo from the Google token.
+ * @param {Env} env
+ * @param {string} token
+ * @returns {Promise<?TokenInfo>}
+ */
+async function getGoogleTokenInfo (env, token) {
+	const r = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+		headers: {
+			authorization: `Bearer ${token}`
+		}
+	})
+	if (!r.ok) {
+		return null
+	}
+	const body = await r.json()
+	if (!body.email) {
+		return null
+	}
+	return {
+		email: body.email,
+		clientId: 'google'
 	}
 }
